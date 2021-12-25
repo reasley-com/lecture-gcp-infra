d4911d272a68da630e2d287a35d89949a353fb09
reasley <33018600+reasley-com@users.noreply.github.com>
2021-12-26 05:18:07 +0900

    feat: 스토리지 실습 첫 번째 코드 추가

---
+++ Source/4 GCP Storage/cloud-sdk-install.sh
@@ -0,0 +1,5 @@
+# Install file download
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-365.0.0-linux-x86_64.tar.gz
+
+# Decompress install file
+tar -zxvf google-cloud-sdk-365.0.0-linux-x86_64.tar.gz
